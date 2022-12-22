5ee2d85a6c2a89026e1889bee2654bb78e1736a6
Melmothe1st <70806614+AdamMonawir@users.noreply.github.com>
2022-12-22 01:01:48 +0000

    web host for replit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@
 import { config } from 'dotenv';
 import { Client, CommandInteraction, GatewayIntentBits, InteractionCollector, Routes } from 'discord.js';
 import { REST } from '@discordjs/rest'
+import express from 'express';
+
+//webhost
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('D.js_v14 is online'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 //.env file
 config();
 
 //intents
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ], 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
+
 //.env file
 const TOKEN = process.env.B_Token
 const CLIENT_ID = process.env.Client_ID
@@ -23,45 +33,46 @@ const GUILD_ID = process.env.GUILD_ID;
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 //login check
-client.once('ready', () =>{console.log(`${client.user.username} has logged in`)});
+client.once('ready', () => { console.log(`${client.user.username} has logged in`) });
 //message check
 //client.on('messageCreate', (message) => {console.log(message.content)});
 
 //Interaction
 client.on('interactionCreate', (interaction) => {
-    if(interaction.isChatInputCommand()) {
-        console.log('Hello World');
-        interaction.reply({ content: `Your order was ${interaction.options.getString('food')} pay up`
+  if (interaction.isChatInputCommand()) {
+    console.log('Hello World');
+    interaction.reply({
+      content: `Your order was ${interaction.options.getString('food')} pay up`
     })
-    }
+  }
 
 });
 
 //Commands
 async function main() {
-    const commands = [
+  const commands = [
+    {
+      name: 'order',
+      description: 'Order something..',
+      options: [
         {
-            name:'order',
-            description: 'Order something..',
-            options: [
-            {
-                name: 'food',
-                description: 'the type of food',
-                type: 3,
-                required: true,
-            }
-        ],
+          name: 'food',
+          description: 'the type of food',
+          type: 3,
+          required: true,
+        }
+      ],
     },
-];
-    try {
-      console.log('Started refreshing application (/) commands.');
-      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands, });
-      
-      //Login
-      client.login(TOKEN);
-    } catch (error) {
-      console.error(error);
-    }
+  ];
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands, });
+
+    //Login
+    client.login(TOKEN);
+  } catch (error) {
+    console.error(error);
   }
+}
 
 main();
